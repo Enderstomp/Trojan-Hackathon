a196846a671adc4e568f089adece6d5b149c111b
Aditya J <adijoshi08@gmail.com>
2024-12-19 10:40:01 -0500

    add typing game

---
+++ Trojan Hackathon/TypingView.swift
@@ -0,0 +1,19 @@
+//
+//  Typing.swift
+//  Trojan Hackathon
+//
+//  Created by Aditya Joshi on 12/19/24.
+//
+import SwiftUI
+
+struct TypingView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    Ball
+}
