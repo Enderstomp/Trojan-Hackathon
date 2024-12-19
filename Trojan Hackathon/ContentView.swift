89ca26b1b426d2b08abb53af4ef5541bb1ab811b
Owen Fahringer <owenfahringer@gmail.com>
2024-12-19 09:27:34 -0500

    Initial Commit

---
+++ Trojan Hackathon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trojan Hackathon
+//
+//  Created by Owen Fahringer on 12/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trojan Hackathon/Trojan_HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trojan_HackathonApp.swift
+//  Trojan Hackathon
+//
+//  Created by Owen Fahringer on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Trojan_HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
